a1b94c813e3ecfe93940421bd0573000a912e388
Owen Buckley <owenbuckley13@gmail.com>
2018-10-28 18:39:47 -0400

    more projects and languages

--- src/data/topology.js
+++ src/data/topology.js
@@ -1,8 +1,14 @@
+import c from './c.topology.json';
+import go from './go.topology.json';
+import java from './java.topology.json';
 import javascript from './javascript.topology.json';
 import php from './php.topology.json';
 
 export default {
   language: {
+    c,
+    go,
+    java,
     javascript,
     php
   }
