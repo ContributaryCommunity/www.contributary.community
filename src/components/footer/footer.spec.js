83e7b215d7add57ce2ba05bb2a8d0ff5e8405ea3
Owen Buckley <owenbuckley13@gmail.com>
2018-11-18 13:25:40 -0500

    added social media links (#39)

--- src/components/footer/footer.spec.js
+++ src/components/footer/footer.spec.js
@@ -17,32 +17,65 @@ describe('Footer Component', () => {
   });
 
   describe('Default Behavior', () => {
-      
-    describe('Link', () => {
-      let link;
 
-      beforeEach(() => {
-        link = footer.shadowRoot.querySelectorAll('footer a')[0];
-      });
+    it('should have one footer element', () => {
+      const footerElement = footer.shadowRoot.querySelectorAll('footer');
 
-      afterEach(() => {
-        link = null;
-      });
-
-      it('should have a href that links to Project Evergreen', () => {   
-        expect(link.href).toBe('https://github.com/ContributaryCommunity');
-      });
-
-      it('should have a title with a meaningful value', () => {   
-        expect(link.title).toBe('Contributary GitHub link');
-      });
-
-      it('should say Contributary', () => {   
-        expect(link.innerHTML).toBe('💙 The Contributary Community');
-      });
-
+      expect(footerElement.length).toBe(1);
     });
-    
+
+    it('should have a message', () => {
+      const message = footer.shadowRoot.querySelectorAll('footer span');
+
+      expect(message.innerHTML).toBeUndefined('💙 The Contributary Community');
+    });
+  });
+
+  describe('Social Media Links', () => {
+    let links;
+    let images;
+
+    beforeEach(() => {
+      links = footer.shadowRoot.querySelectorAll('footer a');
+      images = footer.shadowRoot.querySelectorAll('footer a img');
+    });
+
+    afterEach(() => {
+      links = null;
+      images = null;
+    });
+
+    it('should have 2 links', () => {
+      expect(links.length).toBe(2);
+    });
+
+    it('should have two images', () => {
+      expect(images.length).toBe(2);
+    });
+
+    describe('GitHub Link', () => {
+      const index = 0;
+
+      it('should have a link to the Contributary GitHub repo', () => {
+        expect(links[index].href).toBe('https://github.com/ContributaryCommunity');
+      });
+
+      it('should have an image inside the Contributary GitHub repo link', () => {
+        expect(images[index].alt).toBe('GitHub Logo');
+      });
+    });
+
+    describe('Twitter Link', () => {
+      const index = 1;
+
+      it('should have a link to the Contributary Twitter account', () => {
+        expect(links[index].href).toBe('https://twitter.com/ContributaryCmt');
+      });
+
+      it('should have an image inside the Contributary Twitter account link', () => {
+        expect(images[index].alt).toBe('Twitter Logo');
+      });
+    });
   });
 
 });
