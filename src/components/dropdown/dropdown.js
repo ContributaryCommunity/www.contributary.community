8abd3ec7cf022d0973b142555e4bd8df3f83d63e
Grant Hutchinson <h.g.utchinson@gmail.com>
2019-01-15 19:50:34 -0500

    Adding proposed theme with new themeable dropdowns (#43)
    * Adding propose theme with new themeable dropdowns
    
    * Fixing checkbox boolean bind, refactoring
    
    * Refactoring, removing redundancies
    
    * Fixing missing event removal on disconnect
    
    * Fixing dynamic width of selected option bug
    
    * Fixing test coverage
    
    * update threshold

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,41 +1,101 @@
-import { html, LitElement } from '@polymer/lit-element'; 
+import { html, LitElement } from '@polymer/lit-element';
+import css from './dropdown.css';
 
 class DropdownComponent extends LitElement {
-
   static get properties() {
     return {
-      optionSelectedCallback: {
-        type: Function
-      },
       label: {
         type: String
       },
       options: {
         type: Array
+      },
+      selected: {
+        type: String
       }
     };
   }
 
-  /* eslint-disable indent */
-  render() {
-    let { options, label, optionSelectedCallback } = this;
-    
-    if (!options) {
-      options = [];
-    } 
-    
-    return html`
-      <select @change="${optionSelectedCallback}">
-        <option value="">${label}</option>
+  handleDropdownClickEvent(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    let selected = '';
 
-        ${options.map((option) => {
-            return html`<option value="${option.value}">${option.label}</option>`;
-          })
-        }                        
-      </select>
+    if (e.target.htmlFor) {
+      selected = e.target.parentNode;
+      if (e.target.htmlFor !== 'option_def') {
+        this.setSelectedDropdownOption(this.shadowRoot.querySelector('#' + e.target.htmlFor).value);
+        selected.classList.add('selection');
+        this.shadowRoot.querySelector('[for="option_def"]').textContent = e.target.textContent;
+      }
+    } else {
+      selected = e.target;
+    }
+    this.dropdown = selected;
+    selected.classList.toggle('expanded');
+  }
+
+  setSelectedDropdownOption(selected) {
+    let idx = this.options.findIndex(({ value }) => {
+      return value === selected;
+    });
+
+    let previousCheck = this.options.findIndex(({ checked }) => {
+      return checked;
+    });
+
+    if (previousCheck > -1) {
+      this.options[previousCheck].checked = false;
+    }
+
+    if (idx > -1) {
+      this.options[idx].checked = true;
+      this.selected = this.options[idx].value;
+      this.optionSelectedCallback(this.selected, idx);
+    }
+    this.dropdown.scrollTop = 0;
+  }
+
+  connectedCallback() {
+    this.shadowRoot.addEventListener('click', this.handleDropdownClickEvent.bind(this), true);
+    window.addEventListener('click', this.close.bind(this), false);
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.removeEventListener('click', this.handleDropdownClickEvent.bind(this), true);
+    window.removeEventListener('click', this.close.bind(this), false);
+  }
+
+  close() {
+    if (this.dropdown) {
+      this.dropdown.classList.remove('expanded');
+    }
+  }
+
+  renderItems(items, label) {
+    const renderOption = (label, value, checked, id) => html`<input type="checkbox" name="option_drop" ?checked=${checked} value=${value} id=${id}><label for=${id}>${label}</label>`;
+
+    if (typeof label === 'undefined' && items) {
+      return items.map(({ label, value, checked }, idx) => {
+        return renderOption(label, value, checked, `option_${idx}`);
+      });
+    }
+    return renderOption(label, '', false, 'option_def');
+  }
+
+  render() {
+    let { options, label } = this;
+
+    return html`
+    <style>
+      ${css}
+    </style>
+      <span class="dropdown-el">
+        ${this.renderItems(options, label)}
+        ${this.renderItems(options)}
+      </span>
     `;
   }
-  /* eslint-enable indent */
 }
 
 customElements.define('cc-dropdown', DropdownComponent);
--- src/components/dropdown/dropdown.spec.js
+++ src/components/dropdown/dropdown.spec.js
@@ -4,36 +4,6 @@ import './dropdown.js';
 
 describe('Dropdown Component', () => {
 
-  describe('Default Behavior', () => {
-    let dropdown;
-
-    beforeEach(async () => {
-      dropdown = document.createElement('cc-dropdown');
-  
-      document.body.appendChild(dropdown);
-  
-      await dropdown.updateComplete;
-    });
-  
-    afterEach(() => {
-      dropdown.remove();
-      dropdown = null;
-    });
-      
-    it('should have one select element', () => {
-      const select = dropdown.shadowRoot.querySelectorAll('select');
-
-      expect(select.length).toBe(1);
-    });
-
-    it('should have no option elements', () => {
-      const options = dropdown.shadowRoot.querySelectorAll('select options');
-
-      expect(options.length).toBe(0);
-    });
-    
-  });
-
   describe('Passing Multiple Options', () => {
     let testBed;
     let template;
@@ -42,59 +12,63 @@ describe('Dropdown Component', () => {
     const label = 'My Label';
     const options = new Array(numOptions).fill(null).map((key, index) => {
       return {
-        value: `value${index}`
+        value: `value${index}`,
+        label: `label${index}`
       };
     });
+
+    const handleSelection = (value, idx) => {
+      console.log('selected', value);
+      console.log('index', idx);
+    };
 
     beforeEach(async () => {
       testBed = document.createElement('div');
       template = html`
-        <cc-dropdown 
-          label="${label}"
-          .options="${options}"
-        ></cc-dropdown>
+        <cc-dropdown label="${label}" .options="${options}" .optionSelectedCallback="${handleSelection}"></cc-dropdown>
       `;
 
       render(template, testBed);
+      document.body.appendChild(testBed);
       dropdown = testBed.firstElementChild;
 
       await dropdown.updateComplete;
     });
-  
+
     afterEach(() => {
       dropdown.remove();
       dropdown = null;
     });
 
-    it('should have one select element', () => {
-      const select = dropdown.shadowRoot.querySelectorAll('select');
+    it('should have one default element', () => {
+      const select = dropdown.shadowRoot.querySelectorAll('#option_def');
 
       expect(select.length).toBe(1);
     });
 
     it(`should have ${numOptions} options`, () => {
-      const selectOptions = dropdown.shadowRoot.querySelectorAll('select option');
-  
+      const selectOptions = dropdown.shadowRoot.querySelectorAll('.dropdown-el input');
+
       // +1  accounts for default option
       expect(selectOptions.length).toBe(numOptions + 1);
     });
 
     it('first option should have no value', () => {
-      const defaultSelectOption = dropdown.shadowRoot.querySelectorAll('select option')[0];
+      const defaultSelectOption = dropdown.shadowRoot.querySelectorAll('.dropdown-el input')[0];
 
       expect(defaultSelectOption.value).toBe('');
     });
 
     it('should have a label for default option that matches the passed in label', () => {
-      const defaultSelectOption = dropdown.shadowRoot.querySelectorAll('select option')[0];
+      const defaultSelectOption = dropdown.shadowRoot.querySelectorAll('.dropdown-el label')[0];
 
-      expect(defaultSelectOption.innerHTML).toMatch(label);
+      expect(defaultSelectOption.innerText).toMatch(label);
     });
 
     it('should have option values that match the passed in options', () => {
-      const userSelectOptions = Array.from(dropdown.shadowRoot.querySelectorAll('select option')).filter((option) => {
+      const userSelectOptions = Array.from(dropdown.shadowRoot.querySelectorAll('.dropdown-el input')).filter((option) => {
         return option.value !== '';
-      }); 
+      });
 
       userSelectOptions.forEach((option, index) => {
         expect(option.value).toBe(options[index].value);
@@ -102,10 +76,55 @@ describe('Dropdown Component', () => {
       });
     });
 
-    xit('should call the callback function when an option is selected', () => {
-      
+    it('should add a click event listener', () => {
+      const button = dropdown.shadowRoot.querySelector('.dropdown-el');
+      const spyEvent = spyOn(button, 'click');
+
+      button.click();
+
+      expect(spyEvent).toHaveBeenCalled();
     });
 
+    it('should expand on click', () => {
+      const button = dropdown.shadowRoot.querySelector('.dropdown-el');
+
+      button.click();
+      expect(button).toHaveClass('expanded');
+    });
+
+    it('should collapse after an item is clicked', () => {
+      const button = dropdown.shadowRoot.querySelector('.dropdown-el');
+
+      button.click();
+      const selection = button.querySelector('[for="option_0"]');
+
+      selection.click();
+
+      expect(button).not.toHaveClass('expanded');
+    });
+
+    it('should display selected item as default option', () => {
+      const button = dropdown.shadowRoot.querySelector('.dropdown-el');
+
+      button.click();
+      const selection = button.querySelector('[for="option_0"]');
+
+      selection.click();
+
+      const defaultOption = button.querySelector('[for="option_def"]').textContent;
+
+      expect(defaultOption).toEqual(options[0].label);
+    });
+
+    it('should collapse on click anywhere else in the window', () => {
+      const button = dropdown.shadowRoot.querySelector('.dropdown-el');
+
+      button.click();
+
+      document.body.click();
+
+      expect(button).not.toHaveClass('expanded');
+    });
   });
 
 });
