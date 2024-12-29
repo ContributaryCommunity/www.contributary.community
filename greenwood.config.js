b9fb67a1cff0421e5bd6998d088fe8b6c87a35d7
Owen Buckley <owenbuckley13@gmail.com>
2024-12-28 19:16:44 -0500

    upgrade to Lit v3

--- greenwood.config.js
+++ greenwood.config.js
@@ -2,6 +2,31 @@ import { greenwoodPluginGoogleAnalytics } from '@greenwood/plugin-google-analyti
 import { greenwoodPluginImportRaw } from '@greenwood/plugin-import-raw';
 import { greenwoodPluginPolyfills } from '@greenwood/plugin-polyfills';
 import { greenwoodPluginPostCss } from '@greenwood/plugin-postcss';
+import { ResourceInterface } from '@greenwood/cli/src/lib/resource-interface.js';
+
+class ProcessEnvReplaceResource extends ResourceInterface {
+  constructor(compilation) {
+    super();
+
+    this.compilation = compilation;
+  }
+
+  async shouldIntercept(url) {
+    return url.pathname.endsWith('redux.mjs');
+  }
+
+  async intercept(url, request, response) {
+    const body = await response.text();
+    const env = process.env.__GWD_COMMAND__ === 'develop' ? 'development' : 'production'; // eslint-disable-line no-underscore-dangle
+    const contents = body.replace(/process.env.NODE_ENV/g, `"${env}"`);
+
+    return new Response(contents, {
+      headers: new Headers({
+        'Content-Type': 'text/javascript'
+      })
+    });
+  }
+}
 
 export default {
   devServer: {
@@ -16,6 +41,11 @@ export default {
     greenwoodPluginPolyfills(),
     greenwoodPluginGoogleAnalytics({
       analyticsId: 'G-0F85VEMETN'
-    })
+    }),
+    {
+      type: 'resource',
+      name: 'process-env-replace',
+      provider: (compilation) => new ProcessEnvReplaceResource(compilation)
+    }
   ]
 };
