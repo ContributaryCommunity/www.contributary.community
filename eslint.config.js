48a71692a5d428a5ae61bee346d9ccf94c8be44d
Owen Buckley <owenbuckley13@gmail.com>
2025-01-18 14:26:43 -0500

    upgrade eslint v9

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  {
+    // https://github.com/eslint/eslint/discussions/18304#discussioncomment-9069706
+    ignores: [
+      '.greenwood/*',
+      'node_modules/*',
+      'public/*',
+      'www/**'
+    ]
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      // turn this off for Prettier
+      'no-irregular-whitespace': 'off'
+    }
+  }
+];
